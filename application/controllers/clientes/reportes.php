5690000591461dc02df6e0a9417b7ae3811cd345
TTHHLIMA <hernanhuar@techni-translate.com>
2014-01-03 16:25:47 -0500

    confirmacion de ordenes de trabajo

--- application/controllers/clientes/reportes.php
+++ application/controllers/clientes/reportes.php
@@ -1,11 +1,28 @@
 <?php
+
 if (!defined('BASEPATH'))
     exit('HH: no se encontro el directorio');
 
 class Reportes extends CI_Controller {
 
     public function index() {
-        $this->load->view('clientes/reporte');
+        if ($this->session->userdata('Datos_Session')) {
+            $login = $this->session->userdata('Datos_Session');
+            $data['xxxiduser'] = $login['xxxiduser'];
+            $data['xxxnombres'] = $login['xxxnombres'];
+            $data['xxxiniciales'] = $login['xxxiniciales'];
+            $data['xxxnivel'] = $login['xxxnivel'];
+            $data['xxxactivo'] = $login['xxxactivo'];
+            $data['xxxcoordinador'] = $login['xxxcoordinador'];
+            $this->load->vars($data);
+            $this->load->view('marqueting/header/header_compania');
+            $this->load->view('menu/menuSuperior');
+            $this->load->view('clientes/reporte');
+            $this->load->view('footer/footer');
+        } else {
+            //sino inicio sesion redirecciono
+            redirect('login', 'refresh');
+        }
     }
 
 }
